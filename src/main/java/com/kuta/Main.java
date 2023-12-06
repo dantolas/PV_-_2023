3368e05ac4ccff9ec9a0f6af9095f23a41ecb55c
Prince Charming <kuta.samuel@gmail.com>
2023-12-06 11:48:28 +0100

    Project created

---
+++ src/main/java/com/kuta/Main.java
@@ -0,0 +1,7 @@
+package com.kuta;
+
+public class Main {
+  public static void main(String args[]){
+    
+  } 
+}
---
+++ src/main/java/com/kuta/Test.java
@@ -0,0 +1,6 @@
+package com.kuta;
+public class Test {
+    public static void main(String args[]){
+        System.out.println("Test");
+    }
+}
